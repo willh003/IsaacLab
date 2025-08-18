e6b768865607220b32dc455e97d5ca0c4da00140
Will Pinstrup Huey <willhu003@gmail.com>
2025-08-18 11:56:14 -0400

    no frame stack

---
+++ dexterous/dp/commands.sh
@@ -0,0 +1,3 @@
+python train_il.py --algo diffusion_policy --task Isaac-Repose-Cube-Allegro-Contact-Multi-Reset-v0 --dataset /home/wph52/IsaacLab/dexterous/allegro/data/allegro_inhand_multi_rollouts_1000.hdf5
+
+python train_il.py --algo diffusion_policy --task Isaac-Repose-Cube-Allegro-Contact-Multi-Reset-Sparse-v0 --dataset /home/wph52/IsaacLab/dexterous/allegro/data/allegro_inhand_multi_rollouts_1000.hdf5
